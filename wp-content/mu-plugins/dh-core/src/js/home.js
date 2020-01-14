4056abc59e0c93d9716f6e30a3034b69784b325b
Will King <contact@wking.dev>
2020-01-13 20:49:38 -0600

    Donezo

--- wp-content/mu-plugins/dh-core/src/js/home.js
+++ wp-content/mu-plugins/dh-core/src/js/home.js
@@ -1,13 +1,17 @@
 import '../css/home.css';
 
-function handleVideoComplete() {
-  const videoParent = document.querySelector('.hero-video');
-  if (videoParent) {
-    videoParent.setAttribute('data-video-state', 'complete');
-  }
+function handleVideoComplete(video) {
+  return function() {
+    if (video.duration - video.currentTime <= 0.75) {
+      const videoParent = document.querySelector('.hero-video');
+      if (videoParent) {
+        videoParent.setAttribute('data-video-state', 'complete');
+      }
+    }
+  };
 }
 
 const video = document.getElementById('hero-video');
 if (video) {
-  video.addEventListener('ended', handleVideoComplete, false);
+  video.addEventListener('timeupdate', handleVideoComplete(video), false);
 }
